4cf2a690171f443d9e0787955cf805bd59992efb
Martial Anouman <martialanouman@arolitec.com>
2023-05-09 09:44:02 +0000

    chore(prisma): add Book and Author models
    update seed script to include added
    models

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,53 +1,86 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = 'manouman@live.fr'
+  const username = 'd&Wz'
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
-  });
+  })
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash('password', 10)
 
   const user = await prisma.user.create({
     data: {
       email,
+      username,
       password: {
         create: {
           hash: hashedPassword,
         },
       },
     },
-  });
+  })
 
   await prisma.note.create({
     data: {
-      title: "My first note",
-      body: "Hello, world!",
+      title: 'My first note',
+      body: 'Hello, world!',
       userId: user.id,
     },
-  });
+  })
 
   await prisma.note.create({
     data: {
-      title: "My second note",
-      body: "Hello, world!",
+      title: 'My second note',
+      body: 'Hello, world!',
       userId: user.id,
     },
-  });
+  })
 
-  console.log(`Database has been seeded. 🌱`);
+  const author = {
+    name: 'Robert C. Martin',
+    bio: `Robert C. Martin is a professional developer since 1970`,
+  }
+
+  await prisma.author.deleteMany()
+  await prisma.author.create({
+    data: {
+      ...author,
+      creator: { connect: { id: user.id } },
+      books: {
+        create: [
+          {
+            isbn: '978-2-3260-0286-9',
+            creator: { connect: { id: user.id } },
+            summary: 'Pratice clean agile',
+            title: 'Clean Agile',
+            publishedAt: new Date('2021-07-01'),
+          },
+          {
+            isbn: '978-2-3260-0287-9',
+            creator: { connect: { id: user.id } },
+            summary: 'Clean Architecture',
+            title: 'Become a pro of clean software architecture',
+            publishedAt: new Date('2021-05-01'),
+          },
+        ],
+      },
+    },
+  })
+
+  console.log(`Database has been seeded. 🌱`)
 }
 
 seed()
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
